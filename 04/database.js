b89a6dd2ac157eaab4e4b7dba358f77b9d184812
Brooke Hackney <77807845+bdhack23@users.noreply.github.com>
2022-11-29 23:32:34 -0500

    Round 1 of Part 4

---
+++ 04/database.js
@@ -0,0 +1,17 @@
+import Database from 'better-sqlite3';
+
+// **Do I need parameters or are they not required?
+export function tableCheck() {
+  
+// make sure to require database
+const Database = require('Database');
+  
+CREATE TABLE IF NOT EXISTS customers
+(
+   lastname varchar,
+   firstname varchar,
+   email varchar
+);
+  
+// module.exports = tableCheck
+}
