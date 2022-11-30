43703fb28f6be9698286e038a18b1e705f7ca476
Brooke Hackney <77807845+bdhack23@users.noreply.github.com>
2022-11-29 22:51:44 -0500

    Round 1 of Part 2

---
+++ 02/math.js
@@ -0,0 +1,14 @@
+// **Do I need parameters or are they not required?
+export function add() {
+// create the constants for both numbers
+const firstNum = parseInt(prompt('First number '));
+const secondNum = parseInt(prompt('Second number '));
+
+//add two numbers
+const sum = firstNum + secondNum;
+
+// return the sum
+return sum;
+  
+// module.exports = add
+}
