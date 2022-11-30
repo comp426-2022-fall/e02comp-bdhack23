61b96277bed9fea3cf3c0dd5c292fc7bb2641dd1
Brooke Hackney <77807845+bdhack23@users.noreply.github.com>
2022-11-29 21:37:24 -0500

    Round 1 of Part 1

---
+++ 01/server.js
@@ -0,0 +1,24 @@
+// Require fs module
+const fs = require('fs');
+
+const args = require('minimist')(process.argv.slice(2));
+
+// Require minimist module.
+
+// Define a const `port`as 5555
+const port = 5555;
+
+args["message"];
+
+const message = args.message || "It works!";
+
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end(data)
+  })
+  server.listen(message, () => {
+    console.log(`{"message": ${message}}`)
+  });
+});
