def95007bb0a14a9b8a6260d978041df8a389120
Brooke Hackney <77807845+bdhack23@users.noreply.github.com>
2022-11-30 22:13:28 -0500

    Round 1 of Part 8

---
+++ 08/server.js
@@ -0,0 +1,27 @@
+#!/bin/bash
+import fetch from "node-fetch";
+
+// Require http module
+const http = require('http');
+
+// Require fs module
+const fs = require('fs');
+
+const args = require('minimist')(process.argv.slice(2));
+
+args["port"];
+
+const port = args.port || process.env.PORT || 5555;
+
+const time = Date.now();
+
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end(data)
+  })
+  server.listen(time, () => {
+    console.log(`{"unixtime": ${time}}`)
+  });
+});
