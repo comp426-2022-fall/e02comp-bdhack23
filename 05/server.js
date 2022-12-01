9473950f8465716ca670b029e26aa82e0d1afc11
Brooke Hackney <77807845+bdhack23@users.noreply.github.com>
2022-11-30 21:29:44 -0500

    Round 1 of Part 5

---
+++ 05/server.js
@@ -0,0 +1,27 @@
+import Database from 'better-sqlite3';
+
+// Require http module
+const http = require('http');
+
+// Require fs module
+const fs = require('fs');
+
+const args = require('minimist')(process.argv.slice(2));
+
+args["port"];
+
+const port = args.port || process.env.PORT || 5555;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end(data)
+  })
+});
+
+CREATE TABLE IF NOT EXISTS customers
+(
+   lastname varchar,
+   firstname varchar,
+   email varchar
+);
