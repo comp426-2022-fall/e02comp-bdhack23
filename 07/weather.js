fa44613bc6432829b78a2f162dcc4a24fbad1b32
Brooke Hackney <77807845+bdhack23@users.noreply.github.com>
2022-11-30 22:03:52 -0500

    Round 1 of Part 7

---
+++ 07/weather.js
@@ -0,0 +1,24 @@
+#!/bin/bash
+import fetch from "node-fetch";
+const fs = require('fs');
+
+const args = require('minimist')(process.argv.slice(2));
+
+// let latitude = args.latitude; 
+let latitude = process.argv.indexOf('latitude');
+
+// let longitude = args.longitude; 
+let longitude = process.argv.indexOf('longitude');
+
+
+// const url = "https://api.weather.gov/points/36.2168,-81.6746";
+// const url = "https://api.open-meteo.com/v1/forecast?" + "latitude=" + latitude + "&longitude=" + longitude + "&daily=precipitation_sum&timezone=" + tz;
+const url = "https://api.weather.gov/points/?" + latitude + "," + longitude;
+      
+const response = await fetch( url )
+
+const data = await response.json()
+
+console.log( url )
+
+console.log( data )
