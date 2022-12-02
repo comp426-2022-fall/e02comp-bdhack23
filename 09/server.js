02d970769d8a6a72c89dbabdf613f15c54f5123a
Brooke Hackney <77807845+bdhack23@users.noreply.github.com>
2022-12-01 20:58:58 -0500

    Round 1 of Part 9

---
+++ 09/server.js
@@ -0,0 +1,17 @@
+// Require modules
+var fs = require('fs')
+var express = require('express')
+var path = require('path')
+var morgan = require('morgan')
+var app = express()
+
+
+const args = require('minimist')(process.argv.slice(2));
+args["port"];
+
+// Define `port`constant
+const port = args.port || process.env.PORT || 5555;
+
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+
+app.use(morgan('combined', { stream: accessLogStream }))
