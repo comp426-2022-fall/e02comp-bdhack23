19a0d992ca950052348b7534583e61d50073f0ad
Brooke Hackney <77807845+bdhack23@users.noreply.github.com>
2022-12-07 23:56:00 -0500

    fixed brackets

--- 05/server.js
+++ 05/server.js
@@ -17,8 +17,7 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/html')
     res.end(data)
-  })
-});
+  });
 
 CREATE TABLE IF NOT EXISTS customers
 (
