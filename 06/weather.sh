8ffdcc782d5ee17d3e9d1fd2bb4d990759faf57c
Brooke Hackney <77807845+bdhack23@users.noreply.github.com>
2022-12-08 00:39:52 -0500

    got rid of spacing between "="

--- 06/weather.sh
+++ 06/weather.sh
@@ -2,11 +2,11 @@
 const fetch = require'node-fetch'
 
 // # PLACE YOUR CURL COMMAND ON THE LINE BELOW THIS ONE
-const url = "https://api.weather.gov/points/36.2168,-81.6746";
+const url="https://api.weather.gov/points/36.2168,-81.6746";
       
-const response = await fetch( url )
+const response=await fetch( url )
 
-const data = await response.json()
+const data=await response.json()
 
 console.log( url )
 
