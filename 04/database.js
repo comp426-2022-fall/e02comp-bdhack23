556a22a6e12cf5614711dafb3d019c57da3d1bb1
Brooke Hackney <77807845+bdhack23@users.noreply.github.com>
2022-12-07 23:50:23 -0500

    capitalization

--- 04/database.js
+++ 04/database.js
@@ -1,13 +1,11 @@
 const sqlite3 = require('sqlite3').verbose();
 const Database = require('Database');
-const db = new sqlite3.Database( './db.sqlite' );
-
 
 function tableCheck() {
   
 // make sure to require database
   
-CREATE table IF NOT EXISTS customers
+CREATE TABLE IF NOT EXISTS customers
 (
    lastname varchar,
    firstname varchar,
