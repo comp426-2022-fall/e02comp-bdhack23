08a0f2e2c25eb465832f50b4f344da12aac25d4c
Brooke Hackney <77807845+bdhack23@users.noreply.github.com>
2022-12-07 19:53:55 -0500

    changing console.log

--- 02/math.js
+++ 02/math.js
@@ -1,13 +1,13 @@
 // **Do I need parameters or are they not required?
 function add() {
 // create the constants for both numbers
-const firstNum = parseInt(prompt('First number '));
-const secondNum = parseInt(prompt('Second number '));
+const A = parseInt(prompt('First number '));
+const B = parseInt(prompt('Second number '));
 
 //add two numbers
-const sum = firstNum + secondNum;
+const sum = A + B;
 
-console.log(${sum});
+console.log(math.add(${A},${B}));");
 
 // return the sum
 return sum;
