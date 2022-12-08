c8d369b6d6fcc560b9cb79e759da265f4c6e3c2f
Brooke Hackney <77807845+bdhack23@users.noreply.github.com>
2022-12-07 23:20:13 -0500

    require sqlite

--- 04/database.js
+++ 04/database.js
@@ -3,6 +3,7 @@ function tableCheck() {
   
 // make sure to require database
 const Database = require('Database');
+const sqlite3 = require('sqlite3').verbose();
   
 CREATE TABLE IF NOT EXISTS customers
 (
