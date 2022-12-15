e6bacf7d8e91337da1ee76705fdf134670899b3e
Brooke Hackney <77807845+bdhack23@users.noreply.github.com>
2022-12-14 19:12:46 -0500

    trying to fix message input

--- 01/server.js
+++ 01/server.js
@@ -12,12 +12,12 @@ const port = args.port || 5555;
 
 args["message"];
 
-const message = "It works!";
+const message = args.message || "It works!";
 
 
 const server = http.createServer((err, req, res) => {
     res.statusCode = 200
-    // res.setHeader('Content-Type', 'text/html')
+    res.setHeader('Content-Type', 'text/html')
     res.end(data)
 })
 
