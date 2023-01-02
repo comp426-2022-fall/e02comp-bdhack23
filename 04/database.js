ba695a4e4ba9168f2ea614f5db21a4855b48358d
Brooke Hackney <77807845+bdhack23@users.noreply.github.com>
2023-01-02 15:54:51 -0500

    omg

--- 04/database.js
+++ 04/database.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
-// const db = new sqlite3.Database(".info.db");
+const db = new sqlite3.Database(".info.db");
 
-function prepare() {
+function tableCheck() {
   
 // make sure to require database
 const sql = `
