509de001dc5fa5fbb29afe242c3fe8b76154b12e
Brooke Hackney <77807845+bdhack23@users.noreply.github.com>
2023-01-02 16:58:03 -0500

    const

--- 06/weather.sh
+++ 06/weather.sh
@@ -4,7 +4,7 @@ import fetch from 'node-fetch';
 
 # PLACE YOUR CURL COMMAND ON THE LINE BELOW THIS ONE
 
-let url = `https://api.weather.gov/points/36.2168,-81.6746`;
+const url = `https://api.weather.gov/points/36.2168,-81.6746`;
       
 const response = await fetch( url );
 
