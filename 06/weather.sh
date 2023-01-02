74a4092c902ecf9fb1b19efff5d7fde9416ab28f
Brooke Hackney <77807845+bdhack23@users.noreply.github.com>
2023-01-02 16:49:47 -0500

    trying fetch

--- 06/weather.sh
+++ 06/weather.sh
@@ -4,12 +4,21 @@ import fetch from 'node-fetch';
 
 # PLACE YOUR CURL COMMAND ON THE LINE BELOW THIS ONE
 
-let url = `https://api.weather.gov/points/36.2168,-81.6746/forecast`;
+// let url = `https://api.weather.gov/points/36.2168,-81.6746`;
       
-let response=await fetch(url);
+// let response=await fetch(url);
 
-const data=await response.json();
+// const data=await response.json();
 
-console.log( url );
+// console.log( url );
 
-console.log( data );
+// console.log( data );
+
+fetch("https://api.weather.gov/points/36.2168,-81.6746", {
+  "method": "GET",
+  "headers": {
+  }
+  })
+.then(response => {
+  console.log(response);
+})
