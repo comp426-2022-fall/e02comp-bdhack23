a42fd38c5f3cd7da05a02125c8cacf886b2099aa
Brooke Hackney <77807845+bdhack23@users.noreply.github.com>
2023-01-02 14:45:18 -0500

    ..

--- 02/math.js
+++ 02/math.js
@@ -10,8 +10,8 @@ const second = parseInt(prompt('Second number '));
 const sum = math.add(first, second);
 
 // console.log(`math.add(${A},${B})`);
-console.log(`{"message": ${message}}`)
-console.log(`${sum}`);
+// console.log(`${sum}`);
+console.log(sum);
 
 // return the sum
 return sum;
